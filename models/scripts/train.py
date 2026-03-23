32a56277a5d8c2e3c6555626c650ba85e0741195
cic ipn <abdullah2025@cic.ipn.mx>
2026-03-23 15:10:15 -0600

    Update train.py

--- models/scripts/train.py
+++ models/scripts/train.py
@@ -1,4 +1,5 @@
 import torch
+import yaml
 from torch.utils.data import DataLoader
 from models.eeg_encoder import EEGEncoder
 from models.text_encoder import TextEncoder
@@ -6,37 +7,68 @@ from models.alignment import CLEP, DomainAdversarial
 from models.fusion import HypernetworkFusion
 from models.classifier import ClassifierHead
 from training.losses import CombinedLoss
-from training.metrics import compute_metrics
 from data.dataset import MultimodalDataset
 
-def train_one_epoch(model, dataloader, optimizer, device):
-    model.train()
-    total_loss = 0.0
-    for batch in dataloader:
-        eeg_signal, eeg_spec, adj, text, bin_target, ord_target, modality_label = batch
-        eeg_signal = eeg_signal.to(device)
-        eeg_spec = eeg_spec.to(device)
-        adj = adj.to(device)
-        text = text.to(device)
-        bin_target = bin_target.to(device)
-        ord_target = ord_target.to(device)
-        modality_label = modality_label.to(device)
+def main():
+    with open('config/config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
 
-        # Forward
-        z_eeg = eeg_encoder(eeg_signal, eeg_spec, adj)
-        z_text = text_encoder(text)
-        contrast_loss = clep(z_eeg, z_text)
-        adv_loss = dann(z_eeg, modality_label) + dann(z_text, modality_label)
-        # Fusion
-        u_eeg = torch.rand(z_eeg.size(0), device=device)  # placeholder
-        sigma_eeg = torch.rand(z_eeg.size(0), device=device)
-        u_text = torch.rand(z_text.size(0), device=device)
-        sigma_text = torch.rand(z_text.size(0), device=device)
-        z_shared = hyperfusion(z_eeg, z_text, u_eeg, sigma_eeg, u_text, sigma_text)
-        bin_logits, ord_logits = classifier(z_shared)
-        # Loss
-        total_loss, _, _ = combined_loss(bin_logits, ord_logits, bin_target, ord_target, contrast_loss + adv_loss)
-        optimizer.zero_grad()
-        total_loss.backward()
-        optimizer.step()
-    return total_loss.item()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Datasets (implement your data loader)
+    train_dataset = MultimodalDataset(config, split='train')
+    train_loader = DataLoader(train_dataset, batch_size=config['training']['batch_size'], shuffle=True)
+
+    # Models
+    eeg_encoder = EEGEncoder(config['model']).to(device)
+    text_encoder = TextEncoder(config['model']).to(device)
+    clep = CLEP(temperature=config['model']['temperature'])
+    dann = DomainAdversarial(config['model']['d_model'])
+    hyperfusion = HypernetworkFusion(config['model']['d_model'])
+    classifier = ClassifierHead(config['model']['d_model']).to(device)
+    loss_fn = CombinedLoss()
+
+    # Optimizers with different LR
+    optimizer = torch.optim.AdamW([
+        {'params': eeg_encoder.parameters(), 'lr': config['training']['lr_eeg']},
+        {'params': text_encoder.parameters(), 'lr': config['training']['lr_text']},
+        {'params': hyperfusion.parameters(), 'lr': config['training']['lr_align']},
+        {'params': classifier.parameters(), 'lr': config['training']['lr_align']},
+        {'params': clep.parameters(), 'lr': config['training']['lr_align']},
+        {'params': dann.parameters(), 'lr': config['training']['lr_align']}
+    ])
+
+    for epoch in range(config['training']['epochs']):
+        total_loss = 0.0
+        for batch in train_loader:
+            # batch = (eeg_signal, eeg_spec, adj, text, bin_label, ord_label, mod_label)
+            eeg_signal, eeg_spec, adj, text, bin_t, ord_t, mod_t = [x.to(device) for x in batch]
+
+            z_eeg = eeg_encoder(eeg_signal, eeg_spec, adj)
+            z_text = text_encoder(text)
+
+            contrast_loss = clep(z_eeg, z_text)
+            adv_loss = dann(z_eeg, mod_t) + dann(z_text, mod_t)
+
+            # Placeholder uncertainty & complexity (replace with real estimates)
+            u_eeg = sigma_eeg = u_text = sigma_text = torch.ones(z_eeg.size(0), device=device) * 0.1
+
+            z_shared = hyperfusion(z_eeg, z_text, u_eeg, sigma_eeg, u_text, sigma_text)
+            bin_logits, ord_logits = classifier(z_shared)
+
+            loss, _, _ = loss_fn(bin_logits, ord_logits, bin_t, ord_t, contrast_loss + adv_loss)
+
+            optimizer.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(
+                list(eeg_encoder.parameters()) + list(text_encoder.parameters()) +
+                list(hyperfusion.parameters()) + list(classifier.parameters()),
+                config['training']['gradient_clip']
+            )
+            optimizer.step()
+            total_loss += loss.item()
+
+        print(f"Epoch {epoch+1}: Loss = {total_loss/len(train_loader):.4f}")
+
+if __name__ == '__main__':
+    main()
